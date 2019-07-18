2104c794de2b27cc0aa242248fb827732c68f296
Bucky <jacobmoroniolson@gmail.com>
2019-07-18 13:35:58 -0600

    added vim install script

---
+++ setupvim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp .vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
