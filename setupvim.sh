aa466435dfb938af73b8664e16b6d016cc7f7dbc
Jacob Olson <jacobmoroniolson@gmail.com>
2023-05-13 21:19:40 -0600

    updating

--- setupvim.sh
+++ setupvim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp .vimrc ~/.vimrc
+cp dotfiles/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
